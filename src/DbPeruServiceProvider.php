b6c9343189bb0d51c94001e46e1b9a679312b38a
sleepy <ayrtonsc6@gmail.com>
2019-05-13 19:17:08 -0500

    running

--- src/DbPeruServiceProvider.php
+++ src/DbPeruServiceProvider.php
@@ -13,10 +13,12 @@ class DbPeruServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
-        $this->publishes([
-                __DIR__.'/../database/migrations' => database_path('migrations'),
-            ], 'shonen-migrations');
+        
+         if ($this->app->runningInConsole()) {
+            $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+            $this->publishes([__DIR__.'/../database/migrations' => database_path('migrations')]);
+         }
+        
     }
 
     /**
